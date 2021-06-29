6f554333b6226b1d84b60a9192eb35fdcc7c1323
kuropengin <kurokouchi7dream@yahoo.co.jp>
2021-06-29 12:23:11 +0000

    ストリーミング配信apiへの対応

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const { createProxyMiddleware , responseInterceptor } = require('http-proxy-middleware');
+
+const app = express();
+
+const epg_url = process.env.EPGURL;
+const convert_url = process.env.CNVURL;
+
+
+const reg =　new RegExp(epg_url.replace(/\//g, "\/") , 'g')
+
+
+const convert_api = createProxyMiddleware({ 
+  target: epg_url, 
+  changeOrigin: true ,
+  selfHandleResponse: true, 
+  onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+    const response = responseBuffer.toString('utf8');
+    return response.replace(reg, convert_url);
+  })
+});
+
+const normal_api = createProxyMiddleware({ 
+  target: epg_url, 
+  changeOrigin: true
+});
+
+app.use('/api/streams/live/*/m2ts/playlist', convert_api);
+
+app.use('/*', normal_api);
+
+
+module.exports = app;
